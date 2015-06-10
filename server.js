55c06208222a4a283c75d343d51f3c99dc2364f2
Dewey Luu <deweyluu@gmail.com>
2015-06-09 18:30:37 -0700

    added comments for each line

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+//assign express module to variable
 var express = require("express");
+//assign function call of express to variable
 var app = express();
+//sets up an port to listen on
 var port = process.env.PORT || 3000;
 
+//list of cars
 var cars = ["honda", "ford", "bmw", "fiat"];
 
+//access the endpoint url, run a function
 app.get('/cars', function(req, res) {
+//loop through the list of cars
 	for (var i = 0; i < cars.length; i++) {
+//respond with displaying each json object
 	res.json(cars[i]);
 	}
 });
